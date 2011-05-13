c60bdd301d526692687fc777068cca854204d596
AJ Tarachanowicz <ajt@localhype.net>
2011-05-12 23:17:49 -0400

    Reverted changes

--- Admin/Entity/UserAdmin.php
+++ Admin/Entity/UserAdmin.php
@@ -15,13 +15,6 @@ use Sonata\AdminBundle\Admin\Admin;
 
 class UserAdmin extends Admin
 {
-    /**
-     * @orm:Id
-     * @orm:Column(type="integer")
-     * @orm:generatedValue(strategy="AUTO")
-     */
-    protected $id;
-
     protected $maxPerPage = 25;
     
     protected $list = array(
